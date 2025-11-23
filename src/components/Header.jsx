e6bc8dedda03f9b44b3ce5a7d03c262ed67fead6
Burcu Gunes <burcudagli.10@gmail.com>
2025-11-23 20:51:56 +0000

    Navbar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import "../App.css";
 
 function Header() {
   return (
     <header>
-      <h1>My Portfolio</h1>
+      <h1>BURCU GUNES</h1>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
