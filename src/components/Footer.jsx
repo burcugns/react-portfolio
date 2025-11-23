d6e2de545f0f5050849f130bcacead95ba4bc6df
Burcu Gunes <burcudagli.10@gmail.com>
2025-11-23 20:50:26 +0000

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
 function Footer() {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <p className="footer-copy">© 2025 Burcu Gunes. All rights reserved.</p>
+
+      <div className="footer-contact">
+        <div className="footer-links">
+          <a href="https://github.com/burcugns" target="_blank">
+            <i className="fab fa-github"></i>
+            GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/burcu-gns/" target="_blank">
+            <i className="fab fa-linkedin"></i>
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
